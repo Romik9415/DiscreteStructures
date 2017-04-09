e2ada7e103d611ab29f6f5152d4324f3c71cc80a
Romik9415 <hruparoman@gmail.com>
2017-04-09 17:51:09 +0300

    finished lab3

--- app/src/main/java/com/example/romik9415/discretestructures/Lab3.java
+++ app/src/main/java/com/example/romik9415/discretestructures/Lab3.java
@@ -16,9 +16,15 @@ import java.util.Set;
 public class Lab3 extends AppCompatActivity {
     //Створити програмне забезпечення для знаходження булеану довільної нечіткої множини.
 
+    List<String> set;
+    List<Double> vals;
+    List<FuzzySet> fuzzySetSet;
+
     TextView enteredA;
     TextView enteredE;
     TextView result;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +33,12 @@ public class Lab3 extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                getFuzzySet();
             }
         });
 
@@ -43,26 +49,32 @@ public class Lab3 extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //TODO: display
-        List<String> set = new ArrayList<>();
+        set = new ArrayList<>();
         set.add("a");
         set.add("b");
         //set.add("c");
         //set.add("d");
 
-        //TODO: display
-        List<Double> vals = new ArrayList<>();
+        vals = new ArrayList<>();
         vals.add(0d);
         vals.add(.45d);
         vals.add(1d);
 
-        List<FuzzySet> fuzzySetSet = FuzzySet.Boolean(set,vals);
+        displayEnteredData();
+    }
+
+     private void getFuzzySet(){
+        fuzzySetSet = FuzzySet.Boolean(set,vals);
         Log.d("A_L3","Length: "+fuzzySetSet.size()+" expected= "+Math.pow(vals.size(),set.size()));
         for (FuzzySet fuzzySet : fuzzySetSet) {
-            Log.d("A_L3",fuzzySet.toStr()); //TODO: display
+            Log.d("A_L3",fuzzySet.toStr());
+            result.append(fuzzySet.toStr());
         }
+    }
 
-
+    private void displayEnteredData() {
+        enteredA.append(set.toString());
+        enteredE.append(vals.toString());
     }
 
 
